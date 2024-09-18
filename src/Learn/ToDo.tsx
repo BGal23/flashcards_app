17a4390b886ca7d2432e362a4e8f041ddddc92e7
Bartek <bartek.gal23@gmail.com>
2024-09-18 15:59:33 +0200

    todolist

---
+++ src/Learn/ToDo.tsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+const ToDoList = () => {
+  const [contact, setContact] = useState([]);
+
+  const addContact = (item) => {
+    contact.push(item);
+
+    setContact([...contact]);
+    console.log(contact);
+  };
+
+  return (
+    <>
+      <button onClick={() => addContact("new")}>Add contact</button>
+      <br />
+      <br />
+      {contact.map((element, index) => (
+        <div key={index}>
+          {element}
+          <button>Delete</button>
+          <br />
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default ToDoList;
