85368988502e4f8ed23b0037b4b132e1bd24d7e6
Bartek <bartek.gal23@gmail.com>
2024-09-19 20:52:37 +0200

    react generate list test

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import BasicReactComponent from "./Learn/BasicReactComponent/BasicReactComponent";
+import GenerateList from "./Learn/GenerateList/GenerateList";
 
 const App = () => {
   return (
     <>
-      <BasicReactComponent />
-      <br />
-      your code 
+      <GenerateList />
     </>
   );
 };
