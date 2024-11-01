1edb70432d7b843362b96b5272c4ea9219e36e44
Bartek <bartek.gal23@gmail.com>
2024-11-01 12:41:13 +0100

    restart repo

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/learning_react",
+  base: "/flashcards_app",
   plugins: [react()],
 });
